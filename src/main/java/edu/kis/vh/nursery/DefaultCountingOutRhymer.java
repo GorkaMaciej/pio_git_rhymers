7c43a0bf81896abc8688f6078b9d063f947e1224
mkgorka12 <251143@edu.p.lodz.pl>
2025-03-24 15:16:09 +0100

    10. Wygenerowanie gettera dla pola total w pliku DefaultCountingOutRhymer.java

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -9,7 +9,11 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[DEFAULT_CAPACITY];
     private int total = EMPTY_VALUE;
 
-    public void countIn(int in) {
+    public int getTotal() {
+		return total;
+	}
+
+	public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
